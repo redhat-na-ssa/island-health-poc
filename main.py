e4e53822fb82184a367caecfcf63540c09338bd6
bugbiteme <leon.s.levy@gmail.com>
2025-01-28 11:58:14 -0800

    CODE CHANGE

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ service_remote = os.getenv('SERVICE_REMOTE', 'python-rest-api-a')
 
 @app.get("/")
 async def usage():
-    return {"message": "See /docs for API usage"}
+    return {"message": "See /docs for API usage - CODE CHANGE"}
 
 @app.get("/hello")
 async def read_hello():
